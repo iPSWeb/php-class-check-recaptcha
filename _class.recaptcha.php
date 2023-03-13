d3f85b1552dc0887146f56a13a646f1c0863bab3
pligin <pligin103@gmail.com>
2023-03-13 11:36:59 +0300

    Fixed reCaptcha V2 check

--- _class.recaptcha.php
+++ _class.recaptcha.php
@@ -1,36 +1,38 @@
 <?php
-class recaptcha{
+class recaptcha
+{
     var $secret = '';
-    var $url = 'https://www.google.com/recaptcha/api/siteverify';
+    var $_url = 'https://www.google.com/recaptcha/api/siteverify';
     var $response = '';
-    function __construct($response,$secret) {
-        $this->secret = $secret;//секретный ключ
-        $this->response = $response;//ответ из $_POST['g-recaptcha-response']
+    function __construct($response, $secret)
+    {
+        $this->secret = $secret; //секретный ключ
+        $this->response = $response; //ответ из $_POST['g-recaptcha-response']
     }
-    
-    public function checkRecaptcha(){
-        $context  = stream_context_create($this->getOptions());
-        $verify = file_get_contents($this->url,false, $context,-1,40000);
-        $response = json_decode($verify);
-        return $response->success;
+
+    public function checkRecaptcha()
+    {
+        $response = $this->sendRequest($this->getData());
+        return $response['success'];
     }
-    
-    private function getData(){
+
+    private function getData()
+    {
         return http_build_query([
             'secret' => $this->secret,
             'response' => $this->response
         ]);
     }
     
-    private function getOptions(){
-        return [
-            'http' => [
-                'header' => 'Content-Type: application/x-www-form-urlencoded\r\n'.
-                            'Content-Length: '.strlen($this->getData()).'\r\n'.
-                            'User-Agent:MyAgent/1.0\r\n',
-                'method' => 'POST',
-                'content' => $this->getData()
-            ]
-        ];
+    private function sendRequest($content) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_URL, $this->_url . '?' . $content);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 80);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($response, true);
     }
 }
